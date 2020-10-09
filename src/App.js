13f50b97c0d65e170fcd5077ef3a993f2d083113
soulef <soulefmhamdi2@gmail.com>
2020-10-09 22:26:49 +0100

    assignment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import { DISHES } from './shared/dishes';
 import Menu from './components/MenuComponent';
-function App() {
+import { render } from '@testing-library/react';
+
+class  App extends Component {
+
+constructor(props){
+  super(props);
+
+  this.state = {
+    dishes : DISHES
+  }
+}
+
+  render(){
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -11,9 +24,10 @@ function App() {
           <NavbarBrand href="/">Restaurant Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu/>
+      <Menu dishes = {this.state.dishes}/>
     </div>
   );
+  }
 }
 
 export default App;
